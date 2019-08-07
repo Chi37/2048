a72812dd374dc2166783a8c629cfbc9b35c2837c
Chi Huynh <chi@skyhawk-277gt.local>
2019-08-07 09:36:25 -0700

    markdown

---
+++ README.md
@@ -0,0 +1,10 @@
+#2048
+
+
+
+2048 is a single-player sliding block puzzle game designed by Italian web developer Gabriele Cirulli. The game's objective is to slide numbered tiles on a grid to combine them to create a tile with the number 2048. However, one can continue to play the game after reaching the goal, creating tiles with larger numbers.
+    - From [Wikipedia][wiki-link]
+
+   [wiki-link]: https://en.wikipedia.org/wiki/2048_(video_game)
+
+
--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const mapColors =  {
     128: '#ff997c',
     256: '#ffbfad',
     512: '#ff5b2e',
-    1024: 'redorange',
+    1024: 'violetblue',
     2048: 'goldenrod',
 
 };
@@ -27,8 +27,7 @@ const mapColors =  {
 
 
 /*----- cached element references -----*/
-//cache board arrays to look up the individual cells later and check if it is 0 
-
+//cache board arrays to look up the individual cells later and check if it is 0 ?
 let cell = document.querySelectorAll('el') 
 //  var idx = parseInt(evt.target.id.replace('sq', ''));
 
@@ -234,37 +233,51 @@ function rotate(board) {
 
 
 //check end of game after combine
-function checkEndGame(){
-    for (let i = 0; i < board.length; i++){
-        for (let j = 0; j< board[0].length; j++){
-            gameEnd = !board[i][j]? false :true;
+// function checkEndGame(){
+//     for (let i = 0; i < board.length; i++){
+//         for (let j = 0; j< board[0].length; j++){
+//             gameEnd = !board[i][j]? false :true;
            
-        };
-    }
-    return gameEnd;
-};
+//         };
+//     }
+//     return gameEnd;
+// };
 
 
-
-
-
-/** 
- * 
- * let rowAdj = board[0].some((el, idx) => {
-    return el === board[idx + 1];
-    });
-
- */
-
-let rowAdj = 
-    board.forEach(row => {
-        row.some((el, idx) => {
-        if(row[idx+1] === 0){ return true }
-        return el === row[idx + 1];
-        });
-    });
-
-
-
-
-// TODO: DOWN FUNCTION ISN'T PLACING NUM CORRECTLY
+// let board = [
+//     [1, 0, 0, 0],
+//   ];
+  
+//   let canMoveRight = board.some(row => {
+//     // check if two adjacent non-zero values
+//     if (row.some((val, idx) => val && val === row[idx + 1])) return true;
+//     // check if zero is good
+//     let sawNum = false;
+//     return row.some(val => {
+//       if (val) {
+//         sawNum = true;
+//         return false;
+//       } else if (sawNum) {
+//         return true;
+//       } else {
+//         return false;
+//       }
+//     });
+//   });
+  
+//   let canMoveLeft = board.some(row => {
+//     // check if two adjacent non-zero values
+//     if (row.some((val, idx) => val && val === row[idx + 1])) return true;
+//     // check if zero is good
+//     let sawNum = false;
+//     for (let i = 3; i > 0; i--) {
+//       if (row[i]) {
+//         sawNum = true;
+//       } else if (sawNum) {
+//         return true;
+//       }
+//     }
+//     return false;
+//   });
+  
+//   console.log(canMoveRight, canMoveLeft)
